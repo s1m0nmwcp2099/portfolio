3a378c78901f5e8eb47e4ac191ca98f1db553e60
s1m0nmwcp2099 <simonj2099@hotmail.com>
2020-09-06 01:27:31 +0100

    MLFooty8 preparing C# dataframes

--- MLFooty8/GenerateData/Program.cs
+++ MLFooty8/GenerateData/Program.cs
@@ -1,13 +1,56 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
+using Microsoft.Data.Analysis;
 
 namespace GenerateData
 {
     class Program
     {
+        static string connStr = "server = localhost; user = simon; database = football; port = 3306; password = chainsaw";
         static void Main(string[] args)
         {
             //GATHER HISTORICAL MATCH DATA FROM MYSQL TABLE FOR MATCHES THAT INCLUDE HALF TIME SCORES, SHOTS, SHOTS ON TARGET
+            //ThisDiv
+            List<string> _ThisDiv = new List<string>();
+            //Date
+            List<DateTime> _Date = new List<DateTime>();
+            //HomeTeam
+            List<string> _HomeTeam = new List<string>();
+            //AwayTeam
+            List<string> _AwayTeam = new List<string>();
+            //FTHG full time home goals
+            List<int> _FTHG = new List<int>();
+            //FTAG ...away
+            List<int> _FTAG = new List<int>();
+            //FTR ...result
+            List<string> _FTR = new List<string>();
+            //HTHG half time home goals
+            List<int> _HTHG = new List<int>();
+            //HTAG
+            List<int> _HTAG = new List<int>();
+            //HTR
+            List<string> _HTR = new List<string>();
+            //HS shots
+            List<int> _HS = new List<int>();
+            //AwS
+            List<int> _AwS = new List<int>();
+            //HST shots on target
+            List<int> _HST = new List<int>();
+            //AwST
+            List<int> _AwST = new List<int>();
+            //HC corners
+            List<int> _HC = new List<int>();
+            //AC
+            List<int> _AC = new List<int>();
+            string sql = "SELECT * FROM football_data_complete WHERE FTHG IS NOT NULL AND FTAG IS NOT NULL AND FTR IS NOT NULL AND HTHG IS NOT NULL AND HTAG IS NOT NULL AND HTR IS NOT NULL AND HS IS NOT NULL AND AwS IS NOT NULL AND HST IS NOT NULL AND AwST IS NOT NULL AND HC IS NOT NULL AND AC IS NOT NULL;"
+            using (MySqlConnection conn = new MySqlConnection(connStr)){
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read()){
+                    //rdr.GetOrdinal("Date");
+                }
+            }
         }
     }
 }
--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -9,16 +9,6 @@ namespace Sudoku
         static void Main(string[] args)
         {
             //SET UP PUZZLE
-            int[,] possibleCt = new int[9, 9];
-            bool[,,] grid = new bool[9, 9, 9];
-            for (int i = 0; i < 9; i++){
-                for (int j = 0; j < 9; j++){
-                    possibleCt[i, j] = 9;
-                    for (int k = 0; k < 9; k++){
-                        grid[i, j, k] = false;
-                    }
-                }
-            }
             List<string> Lines = new List<string>();
             Lines.Add("2,0,0,7,0,0,0,0,0");
             Lines.Add("5,0,0,0,0,1,0,0,0");
@@ -29,174 +19,51 @@ namespace Sudoku
             Lines.Add("0,9,7,0,0,8,6,0,0");
             Lines.Add("0,0,4,0,0,0,0,1,5");
             Lines.Add("0,0,0,0,0,0,3,0,0");
+            int[,] grid = new int[9, 9];
             for (int i = 0; i < 9; i++){
-                string[] thisLine = Lines[i].Split(',');
+                string[] cells = Lines[i].Split(',');
                 for (int j = 0; j < 9; j++){
-                    int num = Convert.ToInt32(thisLine[j]);
-                    if (num > 0){
-                        grid[i, j, num - 1] = true;
-                        possibleCt[i, j] = 1;
-                    }else{
-                        for (int k = 0; k < 9; k++){
-                            grid[i, j, k] = true;
-                        }
-                    }
+                    grid[i, j] = Convert.ToInt32(cells[j]);
                 }
             }
-            bool puzzleSolved = false;
-            while (puzzleSolved == false){
-                //GO THROUGH EACH SQUARE
-                for (int rw = 0; rw < 9; rw++){
-                    for (int cl = 0; cl < 9; cl++){
-                        //here.Item1
-                        (int, int) here = (rw, cl);
-                        //get vertical, horizontal and subsquare coordinates for comparison
-                        //vertical and horizontal
-                        List<(int, int)> VertTheres = new List<(int, int)>();
-                        List<(int, int)> HorizTheres = new List<(int, int)>();
-                        List<(int, int)> SubTheres = new List<(int, int)>();
-                        for (int i = 0; i < 9; i++){
-                            VertTheres.Add((i, cl));
-                            HorizTheres.Add((rw, i));
-                        }
-                        //subsquare
-                        int subRw = rw / 3;
-                        int subCl = cl / 3;
-                        for (int i = 0; i < 3; i++){
-                            for (int j = 0; j < 3; j++){
-                                SubTheres.Add((subRw * 3 + i, subCl * 3 + j));
-                            }
-                        }
-                        //Compare vertical
-                        for (int i = 1; i <= 9; i++){//check where other('there') square can be i possibilities
-                            List<List<int>> AvNos = new List<List<int>>();
-                            for (int thr = 0; thr < 9; thr++){//each 'there'
-                                if (thr != here.Item1 && possibleCt[VertTheres[thr].Item1, VertTheres[thr].Item2] == i){
-                                    AvNos.Add(new List<int>());
-                                    for (int num = 1; num <= 9; num++){
-                                        if (grid[VertTheres[thr].Item1, VertTheres[thr].Item2, num - 1] == true){
-                                            AvNos[AvNos.Count - 1].Add(num);
-                                        }
-                                    }
-                                }
-                            }
-                            for (int j = 0; j < AvNos.Count - i; j++){
-                                int matches = 0;
-                                for (int k = j + 1; k < AvNos.Count; k++){
-                                    if (AvNos[j].SequenceEqual(AvNos[k])){
-                                        matches++;
-                                    }
-                                }
-                                if (matches >= i){
-                                    for (int l = 0; l < i; l++){
-                                        foreach (int x in AvNos[l]){
-                                            if (grid[here.Item1, here.Item2, x - 1] == true){
-                                                grid[here.Item1, here.Item2, x - 1] = false;
-                                                possibleCt[here.Item1, here.Item2] -= 1;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
 
-                        //Compare horizontal
-                        for (int i = 1; i <= 9; i++){//check where other('there') square can be i possibilities
-                            List<List<int>> AvNos = new List<List<int>>();
-                            for (int thr = 0; thr < 9; thr++){//each 'there'
-                                if (thr != here.Item2 && possibleCt[HorizTheres[thr].Item1, HorizTheres[thr].Item2] == i){
-                                    AvNos.Add(new List<int>());
-                                    for (int num = 1; num <= 9; num++){
-                                        if (grid[HorizTheres[thr].Item1, HorizTheres[thr].Item2, num - 1] == true){
-                                            AvNos[AvNos.Count - 1].Add(num);
-                                        }
+            //SOLVE
+            bool solved = false;
+            while (solved == false){
+                solved = true;
+                //go through each square checking off single numbers
+                for (int i = 0; i < 9; i++){
+                    for (int j = 0; j < 9; j++){
+                        if (grid[i, j] == 0){
+                            List<int> PossibleNos = new List<int>();
+                            for (int x = 1; x <= 9; x++){
+                                PossibleNos.Add(x);
+                            }
+                            for (int k = 0; k < 9; k++){
+                                //check vertically
+                                if (grid[k, j] > 0){
+                                    PossibleNos.Remove(grid[k, j]);
+                                }
+                                //check horizontally
+                                if (grid[i, k] > 0){
+                                    PossibleNos.Remove(grid[i, k]);
+                                }
+                            }
+                            //check subsquare
+                            for (int si = i / 3; si < i / 3 + 3; si++){
+                                for (int sj = j / 3; sj < j / 3 + 3; sj++){
+                                    if (grid[si, sj] > 0){
+                                        PossibleNos.Remove(grid[si, sj]);
                                     }
                                 }
                             }
-                            for (int j = 0; j < AvNos.Count - i; j++){
-                                int matches = 0;
-                                for (int k = j + 1; k < AvNos.Count; k++){
-                                    if (AvNos[j].SequenceEqual(AvNos[k])){
-                                        matches++;
-                                    }
-                                }
-                                if (matches >= i){
-                                    for (int l = 0; l < i; l++){
-                                        foreach (int x in AvNos[l]){
-                                            if (grid[here.Item1, here.Item2, x - 1] == true){
-                                                grid[here.Item1, here.Item2, x - 1] = false;
-                                                possibleCt[here.Item1, here.Item2] -= 1;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        //Compare sub-square
-                        for (int i = 1; i <= 9; i++){//check where other('there') square can be i possibilities
-                            List<List<int>> AvNos = new List<List<int>>();
-                            /*for (int thr = 0; thr < 9; thr++){//each 'there'
-                                if (thr != here.Item2 && possibleCt[HorizTheres[thr].Item1, HorizTheres[thr].Item2] == i){
-                                    AvNos.Add(new List<int>());
-                                    for (int num = 1; num <= 9; num++){
-                                        if (grid[HorizTheres[thr].Item1, HorizTheres[thr].Item2, num - 1] == true){
-                                            AvNos[AvNos.Count - 1].Add(num);
-                                        }
-                                    }
-                                }
-                            }*/
-                            //foreach ((int, int) subTp in SubTheres){
-                            for (int thr = 0; thr < 9; thr++){
-                                //if ((subTp.Item1 != here.Item1 || subTp.Item2 != here.Item2) && possibleCt[subTp.Item1, subTp.Item2] == i){
-                                if ((SubTheres[thr].Item1 != here.Item1 || SubTheres[thr].Item2 != here.Item2) && possibleCt[SubTheres[thr].Item1, SubTheres[thr].Item2] == i){
-                                    AvNos.Add(new List<int>());
-                                    for (int num = 1; num <= 9; num++){
-                                        if (grid[SubTheres[thr].Item1, SubTheres[thr].Item2, num - 1] == true){
-                                            AvNos[AvNos.Count - 1].Add(num);
-                                        }
-                                    }
-                                }
-                            }
-                            for (int j = 0; j < AvNos.Count - i; j++){
-                                int matches = 0;
-                                for (int k = j + 1; k < AvNos.Count; k++){
-                                    if (AvNos[j].SequenceEqual(AvNos[k])){
-                                        matches++;
-                                    }
-                                }
-                                if (matches >= i){
-                                    for (int l = 0; l < i; l++){
-                                        foreach (int x in AvNos[l]){
-                                            if (grid[here.Item1, here.Item2, x - 1] == true){
-                                                grid[here.Item1, here.Item2, x - 1] = false;
-                                                possibleCt[here.Item1, here.Item2] -= 1;
-                                            }
-                                        }
-                                    }
-                                }
+                            if (PossibleNos.Count == 1){
+                                grid[i, j] = PossibleNos[0];
+                            }else{
+                                solved = false;
                             }
                         }
-                    }
-                }
-                //check if solved and show puzzle so far
-                puzzleSolved = true;
-                for (int rw = 0; rw < 9; rw++){
-                    for (int cl = 0; cl < 9; cl++){
-                        if (possibleCt[rw, cl] > 1){
-                            puzzleSolved = false;
-                            Console.Write("x");
-                        }else{
-                            int x = 0;
-                            for (int num = 1; num <= 9; num++){
-                                if (grid[rw, cl, num - 1] == true){
-                                    x = num;
-                                }
-                            }
-                            Console.Write(x);
-                        }
-                        if (cl < 8){
-                            Console.Write(",");
-                        }
+                        Console.Write(grid[i, j] + " ");
                     }
                     Console.Write("\n");
                 }
