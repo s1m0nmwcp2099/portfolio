c573ac90924d935c37eca633fc6555bbdc52551d
s1m0nmwcp2099 <simonj2099@hotmail.com>
2021-02-27 22:41:58 +0000

    added w,d,l for half and full time

--- MLFooty10/process.py
+++ MLFooty10/process.py
@@ -8,18 +8,24 @@ from datetime import timedelta
 team_averages indexes
 0 = average full time goals scored per game
 1 = average full time goals conceded per game
-2 = average half time goals scored per game
-3 = average half time goals conceded per game
-4 = average shots on target for per game
-5 = average shots on target against per game
-6 = average shots for per game
-7 = average shots against per game
-8 = average corners for per game
-9 = average corners against per game
+2 = average full time wins per game
+3 = average full time draws per game
+4 = average full time losses per game
+5 = average half time goals scored per game
+6 = average half time goals conceded per game
+7 = average half time wins per game
+8 = average half time draws per game
+9 = average half time losses per game
+10 = average shots on target for per game
+11 = average shots on target against per game
+12 = average shots for per game
+13 = average shots against per game
+14 = average corners for per game
+15 = average corners against per game
 """
 
 def team_averages_per_game(df, at_home, this_match_date): # going back 365 days, i = row index - These are time weighted
-    team_averages = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0]
+    team_averages = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
     pld_weight = 0.0 # number of games played represented as time-weighted units
     if at_home == True:
         adj = 0 # adjuster
@@ -33,16 +39,28 @@ def team_averages_per_game(df, at_home, this_match_date): # going back 365 days,
         time_coeff = np.exp(-0.007 * days_gap)
         pld_weight += time_coeff
 
-        team_averages[0] += (df.iloc[i, 4+adj] * time_coeff) # full time goals for
-        team_averages[1] += (df.iloc[i, 5-adj] * time_coeff) # full time goals against
-        team_averages[2] += (df.iloc[i, 6+adj] * time_coeff) # half time goals for
-        team_averages[3] += (df.iloc[i, 7-adj] * time_coeff) # half time goals against
-        team_averages[4] += (df.iloc[i, 8+adj] * time_coeff) # shots on target for
-        team_averages[5] += (df.iloc[i, 9-adj] * time_coeff) # shots on target against
-        team_averages[6] += (df.iloc[i, 10+adj] * time_coeff)# shots for
-        team_averages[7] += (df.iloc[i, 11-adj] * time_coeff)# shots against
-        team_averages[8] += (df.iloc[i, 12+adj] * time_coeff)# corners for
-        team_averages[9] += (df.iloc[i, 13-adj] * time_coeff)# corners against
+        team_averages[0] += (df.iloc[i, 4+adj] * time_coeff)  # full time goals for
+        team_averages[1] += (df.iloc[i, 5-adj] * time_coeff)  # full time goals against
+        if (df.iloc[i, 6]=='H' and at_home==True) or (df.iloc[i, 6]=='A' and at_home==False):
+            team_averages[2] += time_coeff                    # full time win
+        elif df.iloc[i, 6] == 'D':
+            team_averages[3] += time_coeff                    # full time draw
+        elif (df.iloc[i, 6]=='A' and at_home==True) or (df.iloc[i, 6]=='H' and at_home==False):
+            team_averages[4] += time_coeff                    # full time loss
+        team_averages[5] += (df.iloc[i, 7+adj] * time_coeff)  # half time goals for
+        team_averages[6] += (df.iloc[i, 8-adj] * time_coeff)  # half time goals against
+        if (df.iloc[i, 9]=='H' and at_home==True) or (df.iloc[i, 9]=='A' and at_home==False):
+            team_averages[7] += time_coeff                    # full time win
+        elif df.iloc[i, 9] == 'D':
+            team_averages[8] += time_coeff                    # full time draw
+        elif (df.iloc[i, 9]=='A' and at_home==True) or (df.iloc[i, 9]=='H' and at_home==False):
+            team_averages[9] += time_coeff                    # full time loss
+        team_averages[10] += (df.iloc[i, 10+adj] * time_coeff)# shots on target for
+        team_averages[11] += (df.iloc[i, 11-adj] * time_coeff)# shots on target against
+        team_averages[12] += (df.iloc[i, 12+adj] * time_coeff)# shots for
+        team_averages[13] += (df.iloc[i, 13-adj] * time_coeff)# shots against
+        team_averages[14] += (df.iloc[i, 14+adj] * time_coeff)# corners for
+        team_averages[15] += (df.iloc[i, 15-adj] * time_coeff)# corners against
 
     for i in range (0, len(team_averages)):
         team_averages[i] /= pld_weight
@@ -80,16 +98,21 @@ for i in range (0, len(df_raw.index)):
     this_date = df_raw.iloc[i]['Date']
     one_year_earlier = this_date - timedelta(days=365)
     if this_date > (earliest + timedelta(days=365)):
+        # calculate home averages
         home_team = df_raw.iloc[i]['HomeTeam']
         df_home_team_prev_at_home = df_raw.loc[(df_raw['HomeTeam']==home_team) & (df_raw['Date']<this_date) & (df_raw['Date']>one_year_earlier)]
         home_team_home_averages = team_averages_per_game(df_home_team_prev_at_home, True, this_date)
         df_home_team_prev_at_away = df_raw.loc[(df_raw['AwayTeam']==home_team) & (df_raw['Date']<this_date) & (df_raw['Date']>one_year_earlier)]
         home_team_away_averages = team_averages_per_game(df_home_team_prev_at_away, False, this_date)
 
+        # calculate away averages
         away_team = df_raw.iloc[i]['AwayTeam']
         df_away_team_prev_at_away = df_raw.loc[(df_raw['AwayTeam']==away_team) & (df_raw['Date']<this_date) & (df_raw['Date']>one_year_earlier)]
         away_team_away_averages = team_averages_per_game(df_away_team_prev_at_away, False, this_date)
         df_away_team_prev_at_home = df_raw.loc[(df_raw['HomeTeam']==away_team) & (df_raw['Date']<this_date) & (df_raw['Date']>one_year_earlier)]
         away_team_home_averages = team_averages_per_game(df_away_team_prev_at_home, True, this_date)
         
-        
+        # calculate league averages
+        df_lg_year = df_raw.loc[(df_raw['Date']<this_date) & (df_raw['Date']>one_year_earlier)]
+        home_all_av = team_averages_per_game(df_lg_year, True, this_date)
+        away_all_av = team_averages_per_game(df_lg_year, False, this_date)
